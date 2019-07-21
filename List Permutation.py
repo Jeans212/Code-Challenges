8b499f3add525d59d5f0f061b6cccca5bd7719e5
Oluwafemi John Semako <33866480+Jeans212@users.noreply.github.com>
2019-07-21 11:38:33 +0100

    Create List Permutation.py
    This solution was suggested by Anatoly Alekseev
    https://stackoverflow.com/users/4334120/anatoly-alekseev

---
+++ List Permutation.py
@@ -0,0 +1,19 @@
+#Generating all possible permutations from a list, given the no of choices
+
+#Given a list n, and number of choices r, in how many ways can it be arranged?
+#List the permutations..
+
+def list_perm(n, r):
+    a = [[i for i in range(0)]]
+    for i in range(r):
+        a = [[b] + c for b in n for c in a if (b in c) == False]
+    print('There are %s possible ways! \n' %len(a))
+    return a
+
+
+
+# >>> list_perm([1,2,3], 2)
+#There are 6 ways 
+#
+#[[1, 2], [1, 3], [2, 1], [2, 3], [3, 1], [3, 2]]
+ 
